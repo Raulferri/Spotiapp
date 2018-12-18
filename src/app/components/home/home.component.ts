74b0f2d92362be677efa8cd4ad7c2e9342ff7c00
Raulferri <rferribisquert@gmail.com>
2018-12-18 13:39:19 +0100

    1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,10 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  paises: any[] = [];
+  //paises: any[] = [];
 
   //constructor( private http: HttpClient ) 
-  constructor()
+  constructor(private spotify: SpotifyService)
   { 
     /*console.log('Constructor del Home Hecho');
     this.http.get('https://restcountries.eu/rest/v2/lang/es')
